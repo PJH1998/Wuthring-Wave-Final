e402beb826f0039d29f4bc3e5e2cca80a5ee907d
PJH1998 <135578483+PJH1998@users.noreply.github.com>
2025-10-02 16:21:33 +0900

    #ADD : bat 파일

---
+++ Copy_D.bat
@@ -0,0 +1,6 @@
+xcopy		/y/s	.\Engine\Public\*.*		.\EngineSDK\Inc\
+
+xcopy		/y		.\Engine\Bin\Debug\Engine.dll	.\Client\Bin\Debug\
+xcopy		/y		.\Engine\Bin\Debug\Engine.dll	.\Editor\Bin\Debug\
+xcopy		/y		.\Engine\Bin\Debug\Engine.lib	.\EngineSDK\Lib\Debug\
+xcopy		/y		.\Engine\Bin\ShaderFiles\*.*		.\Client\Bin\ShaderFiles\
---
+++ Copy_R.bat
@@ -0,0 +1,6 @@
+xcopy		/y/s	.\Engine\Public\*.*			.\EngineSDK\Inc\
+
+xcopy		/y		.\Engine\Bin\Release\Engine.dll	.\Client\Bin\Release\
+xcopy		/y		.\Engine\Bin\Release\Engine.dll	.\Editor\Bin\Release\
+xcopy		/y		.\Engine\Bin\Release\Engine.lib	.\EngineSDK\Lib\Release\
+xcopy		/y		.\Engine\Bin\ShaderFiles\*.*		.\Client\Bin\ShaderFiles\
