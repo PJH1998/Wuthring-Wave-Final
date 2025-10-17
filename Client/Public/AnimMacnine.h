7c7d405ee2266dee4e511b456607d78ed34f14d9
arch <jin960504@naver.com>
2025-10-17 10:46:13 +0900

    #Temp AnimState

---
+++ Client/Public/AnimMacnine.h
@@ -0,0 +1,42 @@
+#pragma once
+#include "Component.h"
+
+NS_BEGIN(Engine)
+class CModel;
+NS_END
+
+NS_BEGIN(Client)
+class CAnimMacnine final : public CComponent
+{
+	typedef struct tagAnimMacnineDesc
+	{
+		_uint*			pOwnerState;
+	}ANIMMACNINE_DESC;
+
+private:
+	explicit CAnimMacnine(ID3D11Device* pDevice, ID3D11DeviceContext* pContext);
+	explicit CAnimMacnine(const CAnimMacnine& Prototype);
+	virtual ~CAnimMacnine() = default;
+
+public:
+	virtual HRESULT		Initialize_Prototype();
+	virtual HRESULT		Initialize_Clone(void* pArg);
+
+	void Handle_Input(CModel* pModelCom, _uint iIndex);
+	void Update(_float fTimeDelata, CModel* pModelCom);
+	
+	void Reset();
+
+private:
+	_uint* m_pOwnerState = { nullptr };
+	_string m_strCurrentAnimTag;
+	vector<class CAnimState*> m_AnimStates;
+	_uint m_iCurrentStateIndex{};
+
+public:
+	static CAnimMacnine* Create(ID3D11Device* pDevice, ID3D11DeviceContext* pContext);
+	virtual CAnimMacnine* Clone(void* pArg) override;
+	virtual void Free() override;
+
+};
+NS_END
