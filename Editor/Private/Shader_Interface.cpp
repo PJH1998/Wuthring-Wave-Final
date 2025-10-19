6f102bb5f715bbe527009207b14bdc5a7b106081
Kimjunghun <myhoon1015@gmail.com>
2025-10-19 20:08:51 +0900

    feat : Dummy

--- Editor/Private/Shader_Interface.cpp
+++ Editor/Private/Shader_Interface.cpp
@@ -20,24 +20,47 @@ void CShader_Interface::Set_ShadowBias()
 {
 	ImGui::Begin("SHADOW BIAS");
 
-	ImGui::CollapsingHeader("Base Bias");
+	
+	if(ImGui::CollapsingHeader("Base Bias"))
 	{
-		ImGui::InputFloat("BIAS_CSM[0]", &m_fBias[0]);
-		ImGui::InputFloat("BIAS_CSM[1]", &m_fBias[1]);
-		ImGui::InputFloat("BIAS_CSM[2]", &m_fBias[2]);
-		ImGui::InputFloat("BIAS_CSM[3]", &m_fBias[3]);
+		Setting_Bias("BIAS_CASCADE[0]", &m_fBias[0]);
+		Setting_Bias("BIAS_CASCADE[1]", &m_fBias[1]);
+		Setting_Bias("BIAS_CASCADE[2]", &m_fBias[2]);
+		Setting_Bias("BIAS_CASCADE[3]", &m_fBias[3]);
+	}
+
+	if (ImGui::CollapsingHeader("Min Bias"))
+	{
+		Setting_Bias("MIN_BIAS_CASCADE[0]", &m_fMinBias[0]);
+		Setting_Bias("MIN_BIAS_CASCADE[1]", &m_fMinBias[1]);
+		Setting_Bias("MIN_BIAS_CASCADE[2]", &m_fMinBias[2]);
+		Setting_Bias("MIN_BIAS_CASCADE[3]", &m_fMinBias[3]);
 	}
 
 	ImGui::InputFloat("SLOPE_SCALE", &m_fSlopeScale);
 	
 
-	if(ImGui::Button("Apply"))
-	{
-		m_pGameInstance->Bind_RawValue_Renderer("g_ShadowBias", &m_fBias, sizeof(_float4));
-		m_pGameInstance->Bind_RawValue_Renderer("g_DebugSlopeScale", &m_fSlopeScale, sizeof(_float));
-	}
-
+	m_pGameInstance->Bind_RawValue_Renderer("g_fShadowBais", &m_fBias, sizeof(_float4));
+	m_pGameInstance->Bind_RawValue_Renderer("g_fMinShadowBias", &m_fMinBias, sizeof(_float4));
+	m_pGameInstance->Bind_RawValue_Renderer("g_DebugSlopeScale", &m_fSlopeScale, sizeof(_float));
+	
 	ImGui::End();
+}
+
+void CShader_Interface::Setting_Bias(const _char* pName, _float* pFloat)
+{
+	ImGui::PushItemWidth(250.f);
+	ImGui::DragFloat(pName, pFloat, 0.001f, 0.001f, 0.5f);
+
+//	ImGui::SliderFloat(pName, pFloat, 0.001f, 0.5f);
+	ImGui::PopItemWidth();
+
+	//ImGui::SameLine();
+
+	//ImGui::PushItemWidth(120.f);
+
+	//ImGui::InputFloat("", pFloat, 0.001f, 0.01f);
+	//ImGui::PopItemWidth();
 }
 
 CShader_Interface* CShader_Interface::Create(ID3D11Device* pDevice, ID3D11DeviceContext* pContext)
