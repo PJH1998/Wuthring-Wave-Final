a582cc45a192ddb50d4133644af5d8411ba6b0aa
PJH1998 <135578483+PJH1998@users.noreply.github.com>
2025-10-19 14:53:48 +0900

    #ADD : SpringCamea 추가중 / Camera 회전 Quaternion화

---
+++ Editor/Public/Camera_Interface.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "Interface_Edit.h"
+
+NS_BEGIN(Editor)
+
+class CCamera_Interface final : public CInterface_Edit
+{
+private:
+	explicit CCamera_Interface(ID3D11Device* pDevice, ID3D11DeviceContext* pContext);
+	virtual ~CCamera_Interface() = default;
+
+public:
+	virtual		HRESULT					Initialize() override;
+
+public:
+	static		CCamera_Interface*	Create(ID3D11Device* pDevice, ID3D11DeviceContext* pContext) override;
+	virtual		void						Free() override;
+};
+
+NS_END
