b112aecd5c9fcf235c3dbe1769f38335dbb86d76
PJH1998 <135578483+PJH1998@users.noreply.github.com>
2025-10-19 15:07:45 +0900

    #HOTFIX

--- Editor/Public/Camera_Interface.h
+++ Editor/Public/Camera_Interface.h
@@ -13,7 +13,7 @@ public:
 	virtual		HRESULT					Initialize() override;
 
 public:
-	static		CCamera_Interface*	Create(ID3D11Device* pDevice, ID3D11DeviceContext* pContext) override;
+	static		CCamera_Interface*	Create(ID3D11Device* pDevice, ID3D11DeviceContext* pContext);
 	virtual		void						Free() override;
 };
 
