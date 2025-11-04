0b94f8c7856ffe37f5280703b97a71906248fd93
Kimjunghun <myhoon1015@gmail.com>
2025-11-04 19:18:28 +0900

    fix : TestLevel

--- Client/Private/Level_Test.cpp
+++ Client/Private/Level_Test.cpp
@@ -24,18 +24,18 @@ HRESULT CLevel_Test::Initialize()
 	// SetUp OctoTree
 	m_pGameInstance->SetUp_OctoTree(_float3(0.f, 0.f, 0.f), _float3(4096, 4096, 4096));
 
-	//TEST
-	SHADOW_MAP_DESC ShadowMapDesc = {};
-	ShadowMapDesc.iNumSectorX = 2;
-	ShadowMapDesc.iNumSectorZ = 2;
-	ShadowMapDesc.iSectorSizeX = 2048;
-	ShadowMapDesc.iSectorSizeZ = 2048;
-	ShadowMapDesc.vCenterPos = _float3(0.f, 0.f, 0.f);
-	ShadowMapDesc.vExtents = _float3(500.f, 250.f, 500.f);
-	ShadowMapDesc.vLightDir = _float3(0.f, -1.f, 0.5f);
+	////TEST
+	//SHADOW_MAP_DESC ShadowMapDesc = {};
+	//ShadowMapDesc.iNumSectorX = 2;
+	//ShadowMapDesc.iNumSectorZ = 2;
+	//ShadowMapDesc.iSectorSizeX = 2048;
+	//ShadowMapDesc.iSectorSizeZ = 2048;
+	//ShadowMapDesc.vCenterPos = _float3(0.f, 0.f, 0.f);
+	//ShadowMapDesc.vExtents = _float3(500.f, 250.f, 500.f);
+	//ShadowMapDesc.vLightDir = _float3(0.f, -1.f, 0.5f);
 
-	if (FAILED(m_pGameInstance->Setting_ShadowMap(ShadowMapDesc)))
-		CRASH("Test");
+	//if (FAILED(m_pGameInstance->Setting_ShadowMap(ShadowMapDesc)))
+	//	CRASH("Test");
 
 	//로더에서 부른 것과 같은 거 부르기.
 	m_pGameSystem->Clone_MapObjects(m_eCurLevel, 0);
@@ -89,18 +89,6 @@ HRESULT CLevel_Test::Initialize()
 	m_pGameInstance->Add_GameObject_ToLayer(iLevel, TEXT("Prototype_TriggerBox"), iLevel, TEXT("Layer_Trigger"), &Tri);
 
 	Ready_Skybox();
-
-	//TEST
-	CShadowMap::SHADOW_MAP_DESC ShadowMapDesc = {};
-	ShadowMapDesc.iNumSectorX = 2;
-	ShadowMapDesc.iNumSectorY = 2;
-	ShadowMapDesc.iSectorSizeX = 2048;
-	ShadowMapDesc.iSectorSizeY = 2048;
-	ShadowMapDesc.vCenterPos = _float3(3000.f, 0.f, 2000.f);
-	ShadowMapDesc.vExtents = _float3(1000.f, 500.f, 1000.f);
-	ShadowMapDesc.vLightDir = _float3(1.f, -0.5f, -1.f);
-
-	//CShadowMap* Test = CShadowMap::Create(m_pDevice, m_pContext, ShadowMapDesc);
 
     return S_OK;
 }
