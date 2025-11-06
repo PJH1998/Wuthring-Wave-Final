bbb8b5242f2c0bf08d6733ce8d7bc7dc8bb04328
PJH1998 <135578483+PJH1998@users.noreply.github.com>
2025-11-06 11:02:08 +0900

    #FIX : Camera 연동 FIX

--- Client/Private/SpringCamera.cpp
+++ Client/Private/SpringCamera.cpp
@@ -278,6 +278,7 @@ void CSpringCamera::Action(_float fTimeDelta)
 		_vector vLerpTranslation = XMVectorLerp(vPreTranslation, vDestTranslation, fRatio);
 		vLerpTranslation = XMVector4Transform(vLerpTranslation, XMLoadFloat4x4(&m_OwnerMatrix));
 		XMStoreFloat4(&m_vLookPosition, XMVectorSetW(XMLoadFloat4(&m_vLookPosition) + vLerpTranslation, 1.f));
+		XMStoreFloat3(&m_vEndTranslation, vDestTranslation);
 
 		// Distance
 		m_fFixedDistance = m_Frames[m_iFrameIndex + 1].fDistance;
@@ -295,6 +296,7 @@ void CSpringCamera::Action(_float fTimeDelta)
 		_vector vDestTranslation = XMLoadFloat3(&m_Frames[m_iFrameIndex + 1].vTranslation);
 		vDestTranslation = XMVector4Transform(vDestTranslation, XMLoadFloat4x4(&m_OwnerMatrix));
 		XMStoreFloat4(&m_vLookPosition, XMVectorSetW(XMLoadFloat4(&m_vLookPosition) + vDestTranslation, 1.f));
+		XMStoreFloat3(&m_vEndTranslation, vDestTranslation);
 
 		m_fFixedDistance = m_fFixedDistance = m_Frames[m_iFrameIndex + 1].fDistance;
 		m_fDistance = m_fFixedDistance;
@@ -330,7 +332,7 @@ void CSpringCamera::SetUp_Recovery()
 		m_isRecovery = true;
 	m_fFixedDistance = m_fPreFixedDistance;
 	XMStoreFloat4(&m_vEndQuaternion, m_pTransformCom->Get_Quaternion());
-	m_vEndTranslation = _float3(0.f, 0.f, 0.f);
+	//m_vEndTranslation = _float3(0.f, 0.f, 0.f);
 }
 
 void CSpringCamera::Ready_Event()
@@ -338,7 +340,10 @@ void CSpringCamera::Ready_Event()
 	m_pGameInstance->Subscribe<CAMERA_ACTION_EVENT>(ENUM_CLASS(STATIC::NONE), TEXT("Event_Camera_Action"), [this](const CAMERA_ACTION_EVENT& event) {
 		if (CAMERA_STATE::ACTION != m_eCameraState && true == event.isAction)
 		{
-			m_OwnerMatrix = event.WorldMatrix;
+			_matrix Matrix = XMLoadFloat4x4(&event.WorldMatrix);
+			_vector vScale{}, vQuat{}, vTranslation{};
+			XMMatrixDecompose(&vScale, &vQuat, &vTranslation, Matrix);
+			XMStoreFloat4x4(&m_OwnerMatrix, XMMatrixRotationQuaternion(vQuat));
 			m_iFrameIndex = -1;
 			m_fTrackPosition = static_cast<_float>(event.iStart);
 			m_fFirstFrame = m_fTrackPosition;
