289d887e2c47978e01976d8bb14da069161f49cb
arch <jin960504@naver.com>
2025-11-07 22:42:01 +0900

    #Add Parry Interaction

--- Client/Private/MonsterTest.cpp
+++ Client/Private/MonsterTest.cpp
@@ -111,7 +111,7 @@ void CMonsterTest::Late_Update(_float fTimeDelta)
 #endif // _DEBUG
 	if(m_fStamina <= 0.f && m_fParalysisAcc >= 5.f)
 		m_isParalysis = true;
-	m_pRigidBodyCom->Sync_Rigidbody(m_pTransformCom);
+	//m_pRigidBodyCom->Sync_Rigidbody(m_pTransformCom);
 	m_pColliderCom->Sync_Position(m_pTransformCom);
 
 	if (FAILED(m_pGameInstance->Add_Render_Object(RENDERGROUP::DYNAMIC, this)))
@@ -268,7 +268,11 @@ void CMonsterTest::Object_Func(const _wstring& wStrObjectTag)
 	}
 	else if (wstrTypeTag == TEXT("Look"))
 	{
-		TurnFix();
+		m_pTransformCom->LookDir(XMLoadFloat3(&m_vTargetDir));
+	}
+	else if (wstrTypeTag == TEXT("LookRev"))
+	{
+		m_pTransformCom->LookDir(XMLoadFloat3(&m_vTargetDir) * -1.f);
 	}
 }
 
@@ -429,8 +433,8 @@ void CMonsterTest::Ready_PartObjects(MONSTERTEST_DESC* pDesc)
 	vector<COLLISIONLAYER> Targets = { COLLISIONLAYER::ATTACK, COLLISIONLAYER::SKILL, COLLISIONLAYER::KNOCKBACK };
 	TriggerDesc.eTargetLayers = Targets;
 	TriggerDesc.pSocketMatrix = m_pModelCom->Get_BoneMatrixPtr(2); // Root
-	TriggerDesc.vExtent = _float3(1.f, 3.f, 1.f);
-	TriggerDesc.vOffsetPos = _float3(0.f, 1.8f, 0.f);
+	TriggerDesc.vExtent = _float3(2.f, 2.f, 2.f);
+	TriggerDesc.vOffsetPos = _float3(0.f, 0.f, -2.f);
 	TriggerDesc.vOffsetRadian = _float3(XMConvertToRadians(0.f), XMConvertToRadians(0.f), XMConvertToRadians(0.f));
 	TriggerDesc.CollisionCallback = [this](_uint iLayer, void* pOther, const ContactManifold& Manifold) {
 		this->ParryEnter(iLayer, pOther, Manifold);
@@ -608,7 +612,7 @@ _bool CMonsterTest::DodgeCooldown()
 
 _bool CMonsterTest::Attack(_uint iIndex, _float fInterval)
 {
-	if (iIndex != 2)
+	if (iIndex != 3)
 		return false;
 	//else
 	//	return false;
