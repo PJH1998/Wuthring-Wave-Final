961cab4c27d6852daa59868778c918a5a19a40e6
XDXDX-99 <dmsql002925@gmail.com>
2025-11-08 17:59:43 +0900

    신왕 이펙트 연동중

--- Client/Private/Level_Test.cpp
+++ Client/Private/Level_Test.cpp
@@ -259,7 +259,8 @@ void CLevel_Test::Ready_MonsterTest()
 
 void CLevel_Test::Ready_Effect()
 {
-    m_pGameSystem->Create_Prefab("../../Client/Bin/Resource/Effect/Prefabs/Common", m_eCurLevel);
+	m_pGameSystem->Create_Prefab("../../Client/Bin/Resource/Effect/Prefabs/Common", m_eCurLevel);
+	m_pGameSystem->Create_Prefab("../../Client/Bin/Resource/Effect/Prefabs/WeiZuoShenWang", m_eCurLevel);
 }
 
 void CLevel_Test::Ready_Skybox()
