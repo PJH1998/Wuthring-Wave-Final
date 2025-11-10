0751c5012dac76ba5dee29a2ed1d5fad1358e741
Kimjunghun <myhoon1015@gmail.com>
2025-11-10 21:52:36 +0900

    feat : Fog Noise

--- Engine/Private/VolumetricFog.cpp
+++ Engine/Private/VolumetricFog.cpp
@@ -42,7 +42,8 @@ HRESULT CVolumetricFog::Initialize(_uint iWinSizeX, _uint iWinSizeY)
 	m_VF_Data.fHegihtFallOff = 0.01f;
 	m_VF_Data.fGroundFallOff = 0.02f;
 	m_VF_Data.fDistanceFallOff = 0.02f;
-	 
+	m_VF_Data.fNoiseScale = 0.002f;
+
 	if (FAILED(Ready_FroxelVolume()))
 		return E_FAIL;
 
@@ -51,6 +52,11 @@ HRESULT CVolumetricFog::Initialize(_uint iWinSizeX, _uint iWinSizeY)
 
 	if (FAILED(Ready_Sampler()))
 		return E_FAIL;
+
+	if (FAILED(Ready_NoiseTexture()))
+		return E_FAIL;
+
+	Make_NoiseTexture();
 
     return S_OK;
 }
@@ -79,9 +85,9 @@ void CVolumetricFog::Add_LightData(const VF_LIGHT& LightData)
 	m_LightDatas.push_back(LightData);
 }
 
-void CVolumetricFog::Update_VF()
+void CVolumetricFog::Update_VF(_float fTimeDelta)
 {
-	Update_Buffer();
+	Update_Buffer(fTimeDelta);
 
 	m_pCS[ENUM_CLASS(CS::VF_LIGHT)]->Set_ConstantBuffer("VF_Data", m_pBuffers[ENUM_CLASS(BUFFER::DATA)]);
 	m_pCS[ENUM_CLASS(CS::VF_LIGHT)]->Set_ConstantBuffer("ShadowMap_Data", m_pGameInstance->Get_ShadowMapDownSampleBuffer());
@@ -89,7 +95,8 @@ void CVolumetricFog::Update_VF()
 	m_pCS[ENUM_CLASS(CS::VF_LIGHT)]->Set_SRV("g_LightDatas", m_pSRVs[ENUM_CLASS(SRV::LIGHT)]);
 	m_pCS[ENUM_CLASS(CS::VF_LIGHT)]->Set_SRV("g_MipDepthTexture", m_pGameInstance->Get_HZB_Resource());
 	m_pCS[ENUM_CLASS(CS::VF_LIGHT)]->Set_SRV("g_ShadowMapTexture", m_pGameInstance->Get_ShadowMapDownSampleSRV());
-	
+	m_pCS[ENUM_CLASS(CS::VF_LIGHT)]->Set_SRV("g_NoiseTexture", m_pSRVs[ENUM_CLASS(SRV::VF_NOISE)]);
+
 	m_pCS[ENUM_CLASS(CS::VF_LIGHT)]->Set_UAV("OutputTexture", m_pUAVs[ENUM_CLASS(UAV::VF_LIGHT)]);
 
 	m_pCS[ENUM_CLASS(CS::VF_LIGHT)]->Set_Sampler(0, m_pDefaultSampler);
@@ -137,13 +144,13 @@ void CVolumetricFog::Setting_VF()
 	ImGui::DragFloat("DISTANCE_FALLOFF", &m_VF_Data.fDistanceFallOff, 0.01f, 0.01f, 1.f, "%.2f");
 	ImGui::DragFloat("HEIGHT_FALLOFF", &m_VF_Data.fHegihtFallOff, 0.01f, 0.01f, 1.f, "%.2f");
 	ImGui::DragFloat("GROUND_FALLOFF", &m_VF_Data.fGroundFallOff, 0.01f, 0.01f, 1.f, "%.2f");
+	ImGui::DragFloat("NOISE_SCALE", &m_VF_Data.fNoiseScale, 0.0001f, 0.00001f, 0.001f, "%.5f");
 
-	
 	ImGui::End();
 }
 #endif
 
-void CVolumetricFog::Update_Buffer()
+void CVolumetricFog::Update_Buffer(_float fTimeDelta)
 {
 #ifdef _DEBUG
 	Setting_VF();
@@ -156,9 +163,8 @@ void CVolumetricFog::Update_Buffer()
 	m_VF_Data.InvViewMatrix = *m_pGameInstance->Get_TransformState_Float4x4_Inv(D3DTS::VIEW);
 	m_VF_Data.InvProjMatrix = *m_pGameInstance->Get_TransformState_Float4x4_Inv(D3DTS::PROJ);
 	m_VF_Data.iLightCount = iLightCount;
-
+	m_VF_Data.fNoiseTimeDelta = fmodf((m_VF_Data.fNoiseTimeDelta + (fTimeDelta * 0.1f)), 1.f);
 	
-
 	//VF_DATA UPDATE
 	D3D11_MAPPED_SUBRESOURCE VF_SubResource = {};
 	m_pContext->Map(m_pBuffers[ENUM_CLASS(BUFFER::DATA)], 0, D3D11_MAP_WRITE_DISCARD, 0, &VF_SubResource);
@@ -175,10 +181,10 @@ void CVolumetricFog::Update_Buffer()
 
 HRESULT CVolumetricFog::Ready_FroxelVolume()
 {
-	if (FAILED(Ready_Texture(ENUM_CLASS(UAV::VF_LIGHT))))
+	if (FAILED(Ready_FogTexture(ENUM_CLASS(UAV::VF_LIGHT))))
 		return E_FAIL;
 
-	if (FAILED(Ready_Texture(ENUM_CLASS(UAV::VF_BEER))))
+	if (FAILED(Ready_FogTexture(ENUM_CLASS(UAV::VF_BEER))))
 		return E_FAIL;
 
 	if (FAILED(Ready_Buffer()))
@@ -187,7 +193,7 @@ HRESULT CVolumetricFog::Ready_FroxelVolume()
     return S_OK;
 }
 
-HRESULT CVolumetricFog::Ready_Texture(_uint iTextureIndex)
+HRESULT CVolumetricFog::Ready_FogTexture(_uint iTextureIndex)
 {
 	if (iTextureIndex >= ENUM_CLASS(UAV::END))
 		return E_FAIL;
@@ -227,6 +233,51 @@ HRESULT CVolumetricFog::Ready_Texture(_uint iTextureIndex)
 	SRVDesc.Texture3D.MipLevels = 1;
 
 	if (FAILED(m_pDevice->CreateShaderResourceView(pTexture, &SRVDesc, &m_pSRVs[iTextureIndex])))
+		CRASH("Failed to Created FV_SRV");
+
+	Safe_Release(pTexture);
+
+	return S_OK;
+}
+
+HRESULT CVolumetricFog::Ready_NoiseTexture()
+{
+	m_vNoiseSize = _float3(64.f, 64.f, 64.f);
+
+	D3D11_TEXTURE3D_DESC TextureDesc = {};
+	TextureDesc.Width = m_vNoiseSize.x;
+	TextureDesc.Height = m_vNoiseSize.y;
+	TextureDesc.Depth = m_vNoiseSize.z;
+	TextureDesc.MipLevels = 1;
+	TextureDesc.Format = DXGI_FORMAT_R8_UNORM;
+	TextureDesc.Usage = D3D11_USAGE_DEFAULT;
+	TextureDesc.BindFlags = D3D11_BIND_UNORDERED_ACCESS | D3D11_BIND_SHADER_RESOURCE;
+	TextureDesc.CPUAccessFlags = 0;
+	TextureDesc.MiscFlags = 0;
+
+	ID3D11Texture3D* pTexture = { nullptr };
+	if (FAILED(m_pDevice->CreateTexture3D(&TextureDesc, nullptr, &pTexture)))
+		CRASH("Failed to Created Texture3D");
+
+	//UAV
+	D3D11_UNORDERED_ACCESS_VIEW_DESC UAVDesc = {};
+	UAVDesc.Format = TextureDesc.Format;
+	UAVDesc.ViewDimension = D3D11_UAV_DIMENSION_TEXTURE3D;
+	UAVDesc.Texture3D.FirstWSlice = 0;
+	UAVDesc.Texture3D.MipSlice = 0;
+	UAVDesc.Texture3D.WSize = TextureDesc.Depth;
+
+	if (FAILED(m_pDevice->CreateUnorderedAccessView(pTexture, &UAVDesc, &m_pUAVs[ENUM_CLASS(UAV::VF_NOISE)])))
+		CRASH("Failed to Created FV_UAV");
+
+	//SRV
+	D3D11_SHADER_RESOURCE_VIEW_DESC SRVDesc = {};
+	SRVDesc.Format = TextureDesc.Format;
+	SRVDesc.ViewDimension = D3D11_SRV_DIMENSION_TEXTURE3D;
+	SRVDesc.Texture3D.MostDetailedMip = 0;
+	SRVDesc.Texture3D.MipLevels = 1;
+
+	if (FAILED(m_pDevice->CreateShaderResourceView(pTexture, &SRVDesc, &m_pSRVs[ENUM_CLASS(UAV::VF_NOISE)])))
 		CRASH("Failed to Created FV_SRV");
 
 	Safe_Release(pTexture);
@@ -292,6 +343,11 @@ HRESULT CVolumetricFog::Ready_ComputeShader()
 	m_pCS[ENUM_CLASS(CS::VF_BEER)] = CComputeShader::Create(m_pDevice, m_pContext, TEXT("../../Engine/Bin/ShaderFiles/Engine_ComputeShader_VF.hlsl"), Macro, "VolumetricFog");
 	ASSERT_CRASH(m_pCS);
 
+	Macro = { { "THREAD_X", "8" }, { "THREAD_Y", "8" }, { "THREAD_Z", "8" }, { NULL, NULL } };
+
+	m_pCS[ENUM_CLASS(CS::VF_NOISE)] = CComputeShader::Create(m_pDevice, m_pContext, TEXT("../../Engine/Bin/ShaderFiles/Engine_ComputeShader_Noise.hlsl"), Macro, "SimpleXNoise");
+	ASSERT_CRASH(m_pCS);
+
 	return S_OK;
 }
 
@@ -322,6 +378,17 @@ HRESULT CVolumetricFog::Ready_Sampler()
 	ASSERT_CRASH(m_pShadowSampler);
 
 	return S_OK;
+}
+
+void CVolumetricFog::Make_NoiseTexture()
+{
+	m_pCS[ENUM_CLASS(CS::VF_NOISE)]->Set_UAV("OutputTexture", m_pUAVs[ENUM_CLASS(UAV::VF_NOISE)]);
+
+	_uint iThreadGroupCountX = static_cast<_uint>((m_vNoiseSize.x + m_vDefinition.x - 1) / m_vDefinition.x);
+	_uint iThreadGroupCountY = static_cast<_uint>((m_vNoiseSize.y + m_vDefinition.y - 1) / m_vDefinition.y);
+	_uint iThreadGroupCountZ = static_cast<_uint>((m_vNoiseSize.z + m_vDefinition.z - 1) / m_vDefinition.z);
+
+	m_pCS[ENUM_CLASS(CS::VF_NOISE)]->Dispatch(iThreadGroupCountX, iThreadGroupCountY, iThreadGroupCountZ);
 }
 
 CVolumetricFog* CVolumetricFog::Create(ID3D11Device* pDevice, ID3D11DeviceContext* pContext, uint iWinSizeX, _uint iWinSizeY)
@@ -355,5 +422,6 @@ void CVolumetricFog::Free()
 	for (_uint j = 0; j < ENUM_CLASS(BUFFER::END); ++j)
 		Safe_Release(m_pBuffers[j]);
 
+	Safe_Release(m_pDefaultSampler);
 	Safe_Release(m_pShadowSampler);
 }
