c9faf17e4dde7901fca479d6a2eaf10dec3fcf4f
arch <jin960504@naver.com>
2025-11-11 20:31:04 +0900

    #Edit AnimMesh Monster Shader Path

--- Client/Bin/ShaderFiles/Shader_VtxAnimMesh.hlsl
+++ Client/Bin/ShaderFiles/Shader_VtxAnimMesh.hlsl
@@ -455,4 +455,15 @@ technique11 DefaultTechnique
         GeometryShader = NULL;
         PixelShader = compile ps_5_0 PS_ROVER();
     }
+
+    pass NormalYellow // 6
+    {
+        SetRasterizerState(RS_Cull_None);
+        SetDepthStencilState(DSS_Default, 0);
+        SetBlendState(BS_Default, float4(0.f, 0.f, 0.f, 0.f), 0xFFFFFFFF);
+
+        VertexShader = compile vs_5_0 VS_MAIN();
+        GeometryShader = NULL;
+        PixelShader = compile ps_5_0 PS_NORMALTEX();
+    }
 }
