2b9832c1cd8357a748fcc8820b91a67e581a95f3
woojooheuk <dnheuk@Naver.com>
2025-11-12 15:54:04 +0900

    #ADD : 신전 내부 장식품들 없는 버전 임시 추가. 돌맹이 날라오는 거 하나 정상화

--- Client/Private/Loader_GamePlay.cpp
+++ Client/Private/Loader_GamePlay.cpp
@@ -95,7 +95,8 @@ HRESULT CLoader_GamePlay::Load_Texture()
 HRESULT CLoader_GamePlay::Load_Model()
 {
 	//m_pGameSystem->Ready_Prototype_Map("../Bin/Resource/Map/MapData/Asphodel_Barrens_1111_dest_Fix/", m_eCurLevel);
-	m_pGameSystem->Ready_Prototype_Map("../Bin/Resource/Map/MapData/The_False_Soerveign_1112_first/", m_eCurLevel);
+	m_pGameSystem->Ready_Prototype_Map("../Bin/Resource/Map/MapData/Asphodel_Barrens_1112_no_deco/", m_eCurLevel);
+	//m_pGameSystem->Ready_Prototype_Map("../Bin/Resource/Map/MapData/The_False_Soerveign_1112_first/", m_eCurLevel);
 
 	// SkyBox
 	_matrix PreTransformMatrix = XMMatrixScaling(0.001f, 0.001f, 0.001f);
