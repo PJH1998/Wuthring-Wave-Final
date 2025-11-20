57bf5cd51f75543cc0730b3359fce6fc541a6683
PJH1998 <135578483+PJH1998@users.noreply.github.com>
2025-11-20 13:46:59 +0900

    #ADD: 천국 리소스

--- Editor/Private/Level_Map.cpp
+++ Editor/Private/Level_Map.cpp
@@ -799,7 +799,8 @@ void CLevel_Map::Load_Objects()
 	//m_FolderPath= "../../Client/Bin/Resource/Map/Logo/";
 	//m_FolderPath = "../../Client/Bin/Resource/Map/The_False_Sovereign/";
 	//m_FolderPath= "../../Client/Bin/Resource/Map/";
-	m_FolderPath = "../../Client/Bin/Resource/Map/Test/Heaven/";
+	//m_FolderPath = "../../Client/Bin/Resource/Map/Test/Heaven/";
+	m_FolderPath = "../../Client/Bin/Resource/Map/Heaven/Box/";
 
     vector<_wstring> m_PrototypeNames;
     vector<_wstring> m_FoliageNames;
