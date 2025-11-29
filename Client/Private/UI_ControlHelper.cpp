a37ce655e9bf9440775f9e5a135c22b6de63e6cb
PJH1998 <135578483+PJH1998@users.noreply.github.com>
2025-11-29 10:30:37 +0900

    #HOTFIX : Release 터지는 현상 수정

--- Client/Private/UI_ControlHelper.cpp
+++ Client/Private/UI_ControlHelper.cpp
@@ -24,7 +24,7 @@ CUI_ControlHelper::CUI_ControlHelper()
 	, m_pGameSystem(CGameSystem::GetInstance())
 {
 	Safe_AddRef(m_pGameInstance);
-	Safe_AddRef(m_pGameSystem);		// 상호참조 발생
+	Safe_AddRef(m_pGameSystem);
 }
 
 HRESULT CUI_ControlHelper::Initialize()
@@ -317,5 +317,5 @@ void CUI_ControlHelper::Free()
 {
 	__super::Free();
 	Safe_Release(m_pGameInstance);
-	Safe_Release(m_pGameSystem);		// 상호참조 발생
+	Safe_Release(m_pGameSystem);
 }
