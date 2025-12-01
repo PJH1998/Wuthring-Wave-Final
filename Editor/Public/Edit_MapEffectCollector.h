73ff27dd395146245b6a48bea8526e9a7bfce3b7
woojooheuk <dnheuk@Naver.com>
2025-12-01 21:58:00 +0900

    #FIX : 맵 수정.

---
+++ Editor/Public/Edit_MapEffectCollector.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+#include"Base.h"
+
+class CEdit_MapEffectCollector final: public CBase
+{
+private:
+	CEdit_MapEffectCollector();
+	virtual ~CEdit_MapEffectCollector() = default;
+
+	HRESULT Initialize();
+	void Set_ImGuiOption();
+
+private:
+	//필요한 거 거 : 좌표, 트리거 이벤트 번호?(안넣었을 때도 가정)
+	//Collaps에 연기는 Collaps에 내가 직접 넣자. Destruction에도 그냥 내가 직접 뼈 위치에 호출하면 될듯?
+
+
+public:
+	static CEdit_MapEffectCollector* Create();
+	virtual void Free()override;
+};
+
