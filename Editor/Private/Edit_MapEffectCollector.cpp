e4e586ba0d4af8893c3befef030fa428865009a7
woojooheuk <dnheuk@Naver.com>
2025-12-02 10:48:35 +0900

    #FIX : 당기는 문 트리거박스 위치 수정.(삭제 고려)

--- Editor/Private/Edit_MapEffectCollector.cpp
+++ Editor/Private/Edit_MapEffectCollector.cpp
@@ -1,13 +1,35 @@
-﻿#include "Edit_MapEffectCollector.h"
+﻿#include"EditorPch.h"
+#include "Edit_MapEffectCollector.h"
+#include"Level_Map.h"
 
 CEdit_MapEffectCollector::CEdit_MapEffectCollector()
 {
 }
 
+HRESULT CEdit_MapEffectCollector::Initialize()
+{
+	return E_NOTIMPL;
+}
+
 void CEdit_MapEffectCollector::Set_ImGuiOption()
 {
+	//ImGui::
+}
+
+CEdit_MapEffectCollector* CEdit_MapEffectCollector::Create()
+{
+	CEdit_MapEffectCollector* pInstance = new CEdit_MapEffectCollector();
+
+	if (FAILED(pInstance->Initialize()))
+	{
+		MSG_BOX("Failed to Create : Edit_MapEffectCollector");
+		Safe_Release(pInstance);
+	}
+
+	return pInstance;
 }
 
 void CEdit_MapEffectCollector::Free()
 {
+	__super::Free();
 }
--- Editor/Public/Edit_MapEffectCollector.h
+++ Editor/Public/Edit_MapEffectCollector.h
@@ -4,6 +4,11 @@
 NS_BEGIN(Editor)
 class CEdit_MapEffectCollector final: public CBase
 {
+public:
+	typedef struct tagEternalEffect {
+		_uint EffectTag = {};
+		_float4 vPos = {};
+	}ETERNAL_EFFECT;
 private:
 	CEdit_MapEffectCollector();
 	virtual ~CEdit_MapEffectCollector() = default;
@@ -15,7 +20,7 @@ private:
 	//필요한 거 거 : 좌표, 트리거 이벤트 번호?(안넣었을 때도 가정)
 	//Collaps에 연기는 Collaps에 내가 직접 넣자. Destruction에도 그냥 내가 직접 뼈 위치에 호출하면 될듯?
 	//얘는 영구 이펙트들 하면 될 거 같은데. 영구이펙트들 폴더 분리 요청?
-
+	map<_uint, ETERNAL_EFFECT> m_EffectInfo;
 public:
 	static CEdit_MapEffectCollector* Create();
 	virtual void Free()override;
