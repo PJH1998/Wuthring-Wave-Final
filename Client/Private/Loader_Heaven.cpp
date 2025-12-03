611192aab7c58d87f998c699eaf84e496e41ad76
XDXDX-99 <dmsql002925@gmail.com>
2025-12-03 15:36:08 +0900

    천국 레벨 레비아탄 이펙트 연동

--- Client/Private/Loader_Heaven.cpp
+++ Client/Private/Loader_Heaven.cpp
@@ -791,7 +791,7 @@ HRESULT CLoader_Heaven::Load_Effect()
 	m_pGameSystem->Load_EffectTexture_FromFolder("../../Client/Bin/Resource/Effect/Prefabs/Common/Texture", m_eCurLevel);
 	m_pGameSystem->Load_EffectMeshDat_FromFolder("../../Client/Bin/Resource/Effect/Prefabs/Common/Dat", m_eCurLevel);
 
-	m_pGameSystem->Create_Effect("../../Client/Bin/Resource/Effect/Prefabs/WeiZuoShenWang", m_eCurLevel);
+	m_pGameSystem->Create_Effect("../../Client/Bin/Resource/Effect/Prefabs/Leviatan", m_eCurLevel);
 
 
 	return S_OK;
