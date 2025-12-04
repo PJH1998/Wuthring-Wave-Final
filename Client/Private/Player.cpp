9e1028e933296fd3ed13033ef82a81605105055e
dkzndk1100 <dkzndk1100@naver.com>
2025-12-04 21:32:38 +0900

    선택되지 않은 캐릭터. QTE Facial 미적용되게 수정.

--- Client/Private/Player.cpp
+++ Client/Private/Player.cpp
@@ -378,6 +378,8 @@ void CPlayer::Player_KeyInput()
 	{
 		m_Characters[m_iCurrentCharacterIdx]->Debug_FullCost(true);
 		m_Characters[m_iCurrentCharacterIdx]->Clear_CoolTime();
+
+		
 	}
 
 
@@ -393,6 +395,7 @@ void CPlayer::Player_KeyInput()
 	if (m_pGameInstance->Get_DIKeyState(DIK_7) == KEYSTATE::UP)
 	{
 		m_Characters[m_iCurrentCharacterIdx]->Get_AbilityCom()->Print_KeySlotinfo();
+		m_Characters[m_iCurrentCharacterIdx]->Spawn_MotionTrail(3.f, 0.5f, 1.f, { 1.f, 1.f, 1.f, 1.f });
 	}
 
 	if (m_pGameInstance->Get_DIKeyState(DIK_8) == KEYSTATE::UP)
