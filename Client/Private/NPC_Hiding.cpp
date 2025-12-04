b3b7b37b90f8f105d4a1d8eaddd8f3900870477e
arch <jin960504@naver.com>
2025-12-04 22:06:59 +0900

    #Edit 코로 패턴 다듬기, 몬스터 모델 머터리얼 추가 작업

--- Client/Private/NPC_Hiding.cpp
+++ Client/Private/NPC_Hiding.cpp
@@ -209,7 +209,12 @@ void CNPC_Hiding::OnCollide_During(_uint iLayer, void* pDesc, const ContactManif
 {
 	if (iLayer == ENUM_CLASS(COLLISIONLAYER::INTERACTION))
 	{
-		
+		SCAN_INFO* pScan = static_cast<SCAN_INFO*>(pDesc);
+		_float fDistance = XMVectorGetX(XMVector3Length(m_pTransformCom->Get_State(STATE::POSITION) - pScan->vCenterPos));
+		if (fDistance <= pScan->fRadius)
+		{
+			//스캔 성공
+		}
 	}
 }
 
