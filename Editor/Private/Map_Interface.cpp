a8d9afee8ac9c92fc42d4ea04649872b1653fecf
XDXDX-99 <dmsql002925@gmail.com>
2025-12-05 17:31:57 +0900

    코로 이펙트 수정, 연동

--- Editor/Private/Map_Interface.cpp
+++ Editor/Private/Map_Interface.cpp
@@ -663,6 +663,9 @@ void CMap_Interface::Ready_Map_Prototype(const _char* pFilePath)
 			if (entry.path().string().find("Anim") != std::string::npos)
 				continue;
 
+			if(entry.path().string().find("FireFly") != std::string::npos)
+				continue;
+
 			if (entry.path().extension() == ".dat") {
 
 				_char FileDrive[MAX_PATH] = {};
