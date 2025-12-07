878f0874c692162137dadf59577d74aefe06e4d0
arch <jin960504@naver.com>
2025-12-07 15:30:30 +0900

    #Edit 테스트 레벨 초기화

--- Client/Private/Level_Test.cpp
+++ Client/Private/Level_Test.cpp
@@ -79,7 +79,7 @@ HRESULT CLevel_Test::Initialize()
     Ready_Layer_Player();
     Ready_Layer_SequnecePlayer();
 	//Ready_Dummy();
-	Ready_MonsterTest();
+	//Ready_MonsterTest();
 	//Ready_CoroSaurus();
 	//Ready_HavocWarrior();
 	//Ready_ElectroPredator();
