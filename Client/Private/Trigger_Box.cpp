f6fb3db30cfb41f3c588b1a39a698ae0fe235578
woojooheuk <dnheuk@Naver.com>
2025-12-11 18:24:54 +0900

    #ADD : 브금 매니저 추가중.

--- Client/Private/Trigger_Box.cpp
+++ Client/Private/Trigger_Box.cpp
@@ -276,8 +276,8 @@ void CTrigger_Box::Register_Trigger()
 			m_pGameInstance->Set_CurrentCamera_Far(600.f);
 			break;
 		case 34:
-			m_pGameSystem->Change_TimeRate(COLLISIONLAYER::PLAYER, 0.1f, 2.f);
-			m_pGameSystem->Change_TimeRate(COLLISIONLAYER::ENEMY, 0.1f, 2.f);
+			m_pGameSystem->Change_TimeRate(COLLISIONLAYER::PLAYER, 0.05f, 2.f);
+			m_pGameSystem->Change_TimeRate(COLLISIONLAYER::ENEMY, 0.05f, 2.f);
 			m_pGameSystem->Play_QTE(_float2(-300.f, 300.f), UI_QTE_TYPE::TRIGGER_ROPE, UI_QTE_BTN::T);
 			break;
 		}
