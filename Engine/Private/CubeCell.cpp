e0b4bc40ac598a911b42b8eea46e8c59e7e2f629
PJH1998 <135578483+PJH1998@users.noreply.github.com>
2025-12-13 16:56:38 +0900

    #FIX : CubeCell Offset 순서 수정

--- Engine/Private/CubeCell.cpp
+++ Engine/Private/CubeCell.cpp
@@ -40,8 +40,8 @@ HRESULT CCubeCell::Initialize(_float3 vCenter, _float3 vExtent, _uint iDepth)
 	{
 		_float3 vOffset = {};
 		vOffset.x = (i & 1) ? 0.5f : -0.5f;
-		vOffset.y = (i & 4) ? -0.5f : 0.5f;
-		vOffset.z = (i & 2) ? 0.5f : -0.5f;
+		vOffset.y = (i & 2) ? 0.5f : -0.5f;
+		vOffset.z = (i & 4) ? -0.5f : 0.5f;
 
 		_float3 vChildCenter = _float3(
 			vCenter.x + vOffset.x * vExtent.x,
