6ba760ff44aebd827f77d03d610d07f5903505e3
DESKTOP-1IV9LVU\Admin <maple331829@gmail.com>
2025-12-14 16:35:53 +0900

    #HOTFIX

--- Client/Public/MapObject_DynamicSound.h
+++ Client/Public/MapObject_DynamicSound.h
@@ -1,5 +1,5 @@
 ﻿#pragma once
-#include "C:\Users\dnheu\source\repos\Wuthring-Wave-Final\EngineSDK\Inc\GameObject.h"
+#include "GameObject.h"
 NS_BEGIN(Client)
 class CMapObject_DynamicSound final: public CGameObject
 {
