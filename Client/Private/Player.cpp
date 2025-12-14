5dd875ea2c25a7102d4208533922e4ac9d95d83b
dkzndk1100 <dkzndk1100@naver.com>
2025-12-14 19:54:42 +0900

    캐릭터 변경 사운드 추가.

--- Client/Private/Player.cpp
+++ Client/Private/Player.cpp
@@ -582,7 +582,8 @@ void CPlayer::Change_Character(CHARACTERTYPE eNextCharacter, _float fTimeDelta)
 		m_iHarmonyCharacterIdx = CHARACTERTYPE::NONE;
 	}
 	
-	
+	// 8. 사운드 재생
+	m_pGameInstance->Play_Sound(TEXT("ui_ia_com_tick (SFX)"), ENUM_CLASS(CHANNEL::PLAYER_UI), 0.5f);
 	//m_pPlayerStatus->Set_CurrentCharIndex(eNextCharacter);
 
 }
