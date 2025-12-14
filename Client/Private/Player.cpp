1cd8b1b1499ea39bf84b9ae10b30ef9d5db472fe
dkzndk1100 <dkzndk1100@naver.com>
2025-12-14 20:38:17 +0900

    로그인 소리 및 위치 방향 조정.

--- Client/Private/Player.cpp
+++ Client/Private/Player.cpp
@@ -54,6 +54,8 @@ HRESULT CPlayer::Initialize_Clone(void* pArg)
     if (FAILED(Ready_Players(pDesc)))
         return E_FAIL;
 
+	
+
     CPlayerFactory::Register_Camera(LEVEL::STATIC, m_eCurLevel, this, m_pGameInstance, &m_pSpringCamera);
     CPlayerFactory::Register_KeyInputs(m_pInputControllerCom, this);
 
@@ -101,7 +103,9 @@ HRESULT CPlayer::Initialize_Clone(void* pArg)
 
 	// 9. 타이머 지정.
 	m_fChangeCoolTime = 3.f;
+
 	
+
 
     return S_OK;
 }
