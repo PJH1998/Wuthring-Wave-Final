a17e2a760c7a6403c1d436b35f77aaee9573d978
DESKTOP-1IV9LVU\Admin <maple331829@gmail.com>
2026-04-20 11:07:11 +0900

    #FIX : FADE 코ㅓ드 수정

--- Engine/Private/Fade.cpp
+++ Engine/Private/Fade.cpp
@@ -20,54 +20,54 @@ void CFade::OnFade(FADE eFade, _float fDuration, function<void()> func)
 	m_Fades.push(FadeDesc);
 }
 
-HRESULT CFade::Initialize(_uint iWinSizeX, _uint iWinSizeY)
+void CFade::Initialize(_uint iWinSizeX, _uint iWinSizeY)
 {
 	// VIBuffer
 	m_pVIBuffer = CVIBuffer_Rect::Create(m_pDevice, m_pContext);
 	ASSERT_CRASH(m_pVIBuffer);
 
 	// Shader
-	m_pShader = CShader::Create(m_pDevice, m_pContext, TEXT("../Bin/ShaderFiles/Engine_Shader_Fade.hlsl"), VTXPOSTEX::Elements, VTXPOSTEX::iNumElements);
+	m_pShader = CShader::Create(m_pDevice, m_pContext, TEXT("../Bin/ShaderFiles/Engine_Shader_Fade.hlsl"), 
+											VTXPOSTEX::Elements, VTXPOSTEX::iNumElements);
 	ASSERT_CRASH(m_pShader);
 
 	// Matrix SetUp
 	_float fWinSizeX = static_cast<_float>(iWinSizeX);
 	_float fWinSizeY = static_cast<_float>(iWinSizeY);
 
-	XMStoreFloat4x4(&m_WorldMatrix, XMMatrixScaling(fWinSizeX, fWinSizeY, 1.f));// *XMMatrixTranslation(fWinSizeX * 0.5f, fWinSizeY * 0.5f, 0.f));
+	XMStoreFloat4x4(&m_WorldMatrix, XMMatrixScaling(fWinSizeX, fWinSizeY, 1.f));
 	XMStoreFloat4x4(&m_ViewMatrix, XMMatrixIdentity());
 	XMStoreFloat4x4(&m_ProjMatrix, XMMatrixOrthographicLH(fWinSizeX, fWinSizeY, 0.f, 1.f));
+}
 
-	return S_OK;
+void CFade::Priority_Update(_float fTimeDelta)
+{
+	if (m_fTimeAcc > m_fDuration)
+		m_isFade = false;
 }
 
 void CFade::Update(_float fTimeDelta)
 {
-	if (m_fTimeAcc >= m_fDuration)
-	{
-		if (false == m_Fades.empty())
-		{
-			FADE_DESC FadeDesc = m_Fades.front();
-			m_eFade = FadeDesc.eFade;
-			m_fDuration = FadeDesc.fDuration;
-			m_Func = FadeDesc.Func;
-			m_isFade = true;
-		}
-		else
-			m_isFade = false;
-
-		m_fTimeAcc = 0.f;
+	if (true == m_Fades.empty())
 		return;
+	else if (false == m_isFade)
+	{
+		m_fTimeAcc = 0.f;
+
+		FADE_DESC FadeDesc = m_Fades.front();
+		m_eFade = FadeDesc.eFade;
+		m_fDuration = FadeDesc.fDuration;
+		m_Func = FadeDesc.Func;
+		m_isFade = true;
 	}
 
 	m_fTimeAcc += fTimeDelta;
 
 	// Fade End
-	if (m_fTimeAcc >= m_fDuration)
+	if (m_fTimeAcc > m_fDuration)
 	{
 		if(nullptr != m_Func)
 			m_Func();
-		m_fDuration = 0.f;
 		m_Fades.pop();
 	}
 }
@@ -90,6 +90,7 @@ void CFade::Bind_Resource()
 	m_pShader->Bind_Matrix("g_ViewMatrix", &m_ViewMatrix);
 	m_pShader->Bind_Matrix("g_ProjMatrix", &m_ProjMatrix);
 
+	m_fDuration = max(m_fDuration, FLT_MIN);
 	_float fTimeRate = m_fTimeAcc / m_fDuration;
 	m_pShader->Bind_Value("g_fTimeRate", &fTimeRate, sizeof(_float));
 }
@@ -97,9 +98,9 @@ void CFade::Bind_Resource()
 CFade* CFade::Create(ID3D11Device* pDevice, ID3D11DeviceContext* pContext, _uint iWinSizeX, _uint iWinSizeY)
 {
 	CFade* pInstance = new CFade(pDevice, pContext);
+	ASSERT_CRASH(pInstance);
 
-	if (FAILED(pInstance->Initialize(iWinSizeX, iWinSizeY)))
-		CRASH("Fade");
+	pInstance->Initialize(iWinSizeX, iWinSizeY);
 
 	return pInstance;
 }
